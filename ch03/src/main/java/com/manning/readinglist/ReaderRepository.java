52e0dec1fe282ffa6a61ec3a7771f6d59e13d248
ericma <924402086@qq.com>
2021-06-18 16:01:56 +0800

    ch02

--- ch03/src/main/java/com/manning/readinglist/ReaderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manning.readinglist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderRepository extends JpaRepository<Reader, String> {
-    
-}
