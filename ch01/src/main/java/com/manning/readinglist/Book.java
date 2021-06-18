a35be7724b8293d988e4c21e41b7f80dfef4634d
ericma <924402086@qq.com>
2021-06-18 16:03:32 +0800

    ch03

---
+++ ch01/src/main/java/com/manning/readinglist/Book.java
@@ -0,0 +1,21 @@
+package com.manning.readinglist;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+    private String description;
+}
